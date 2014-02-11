3f1c8562299bcde8c2e1929608f3ae0af83a8073
Zack whipkey <zack.whipkey@gmail.com>
2014-02-10 21:01:45 -0500

    Fixed assembly name,etc.

--- DAOCAutoConnector/Properties/Settings.Designer.cs
+++ DAOCAutoConnector/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DAOCAutoConnector.Properties {
+namespace DAOCAutoLogin.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
